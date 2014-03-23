1bf5ac79f7cf623147fbbab0283549e7fdf433a6
Douwe De Bock <douwedb26@gmail.com>
2014-03-23 22:30:24 +0100

    module can receive messages and send them back to the client

--- echo-serv.c
+++ echo-serv.c
@@ -15,11 +15,61 @@ struct service {
 
 struct service *svc;
 
+int recv_msg(struct socket *sock, unsigned char *buf, int len) 
+{
+	struct msghdr msg;
+	struct kvec iov;
+	int size = 0;
+
+	printk(KERN_INFO "recv_msg\n");
+
+	iov.iov_base = buf;
+	iov.iov_len = len;
+
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = 0;
+	msg.msg_name = 0;
+	msg.msg_namelen = 0;
+
+	size = kernel_recvmsg(sock, &msg, &iov, 1, len, msg.msg_flags);
+
+	printk(KERN_ALERT "the message is : %s\n",buf);
+
+	return size;
+}
+
+int send_msg(struct socket *sock,char *buf,int len) 
+{
+	struct msghdr msg;
+	struct kvec iov;
+	int size;
+
+	printk(KERN_INFO "send msg\n");
+
+	iov.iov_base = buf;
+	iov.iov_len = len;
+
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = 0;
+	msg.msg_name = 0;
+	msg.msg_namelen = 0;
+
+	size = kernel_sendmsg(sock, &msg, &iov, 1, len);
+
+	printk(KERN_INFO "message sent!\n");
+
+	return size;
+}
+
 int start_listen(void)
 {
 	struct socket *acsock;
-	int error, i;
+	int error, i, size;
 	struct sockaddr_in sin;
+	int len = 15;
+	unsigned char buf[len+1];
 
 	error = sock_create_kern(PF_INET,SOCK_STREAM,IPPROTO_TCP,&svc->listen_socket);
 	if(error<0) {
@@ -53,6 +103,11 @@ int start_listen(void)
 			goto wait;
 		}
 		printk(KERN_ERR "sock %d accepted\n", i++);
+
+		memset(&buf, 0, len+1);
+		size = recv_msg(acsock, buf, len);
+		send_msg(acsock, buf, size);
+
 		kernel_sock_shutdown(acsock, SHUT_RDWR);
 		sock_release(acsock);
 	}
@@ -71,7 +126,7 @@ wait:
 static int __init mod_init(void)
 {
 	svc = kmalloc(sizeof(struct service), GFP_KERNEL);
-	svc->thread = kthread_run((void *)start_listen, NULL, "ktcp");
+	svc->thread = kthread_run((void *)start_listen, NULL, "echo-serv");
 	printk(KERN_ALERT "echo-serv module loaded\n");
 
         return 0;
