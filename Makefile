f0bcd698c3e4d22169c53ec53a57fc4225c505cf
Douwe De Bock <douwedb26@gmail.com>
2014-03-24 14:44:02 +0100

    added client module

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 obj-m += echo-serv.o
+obj-m += echo-client.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- echo-client.c
+++ echo-client.c
@@ -3,13 +3,14 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 
 #include <net/sock.h>
 
 #define PORT 2325
 
 struct service {
-	struct socket *listen_socket;
+	struct socket *socket;
 	struct task_struct *thread;
 };
 
@@ -32,8 +33,7 @@ int recv_msg(struct socket *sock, unsigned char *buf, int len)
 
 	size = kernel_recvmsg(sock, &msg, &iov, 1, len, msg.msg_flags);
 
-	if (size > 0)
-		printk(KERN_ALERT "the message is : %s\n",buf);
+	printk(KERN_ALERT "the message is : %s\n",buf);
 
 	return size;
 }
@@ -55,62 +55,47 @@ int send_msg(struct socket *sock,char *buf,int len)
 
 	size = kernel_sendmsg(sock, &msg, &iov, 1, len);
 
-	if (size > 0)
-		printk(KERN_INFO "message sent!\n");
+	printk(KERN_INFO "message sent!\n");
 
 	return size;
 }
 
-int start_listen(void)
+int start_sending(void)
 {
-	struct socket *acsock;
-	int error, i, size;
+	int error, i, size, ip;
 	struct sockaddr_in sin;
 	int len = 15;
 	unsigned char buf[len+1];
+	const char text[] = "hello world!\n";
 
-	error = sock_create_kern(PF_INET,SOCK_STREAM,IPPROTO_TCP,&svc->listen_socket);
-	if(error<0) {
-		printk(KERN_ERR "cannot create socket\n");
-		goto wait;
-	}
-
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	ip = (10 << 24) | (1 << 16) | (1 << 8) | (3);
+	sin.sin_addr.s_addr = htonl(ip);
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(PORT);
 
-	error = kernel_bind(svc->listen_socket,(struct sockaddr*)&sin,sizeof(sin));
-	if(error < 0) {
-		printk(KERN_ERR "cannot bind socket, error code: %d\n", error);
-		goto wait;
-	}
-
-	error = kernel_listen(svc->listen_socket,5);
-	if(error<0) {
-		printk(KERN_ERR "cannot listen to socket, error code: %d\n", error);
-		goto wait;
-	}
-
-	acsock = kmalloc(sizeof(struct socket), GFP_KERNEL);
 	i = 0;
 	while (!kthread_should_stop()) {
-		error = kernel_accept(svc->listen_socket, &acsock, 0);
+		error = sock_create_kern(PF_INET,SOCK_STREAM,IPPROTO_TCP,&svc->socket);
 		if(error<0) {
-			printk(KERN_ERR "cannot accept socket\n");
-			kfree(acsock);
+			printk(KERN_ERR "cannot create socket\n");
 			goto wait;
 		}
-		printk(KERN_ERR "sock %d accepted\n", i++);
 
-		memset(&buf, 0, len+1);
-		while ((size = recv_msg(acsock, buf, len)) > 0) {
-			send_msg(acsock, buf, size);
-			memset(&buf, 0, len+1);
+		error = kernel_connect(svc->socket, (struct sockaddr*)&sin, sizeof(struct sockaddr_in), 0);
+		if(error<0) {
+			printk(KERN_ERR "cannot connect socket\n");
+			goto wait;
 		}
+		printk(KERN_ERR "sock %d connected\n", i++);
 
-		sock_release(acsock);
+		strncpy((char*)&buf, &text[0], 14);
+		send_msg(svc->socket, buf, 13);
+		size = recv_msg(svc->socket, buf, len);
+
+		kernel_sock_shutdown(svc->socket, SHUT_RDWR);
+		msleep(1000);
 	}
-	kfree(acsock);
+	sock_release(svc->socket);
 wait:
 	set_current_state(TASK_INTERRUPTIBLE);
 	while (!kthread_should_stop()) {
@@ -125,30 +110,30 @@ wait:
 static int __init mod_init(void)
 {
 	svc = kmalloc(sizeof(struct service), GFP_KERNEL);
-	svc->thread = kthread_run((void *)start_listen, NULL, "echo-serv");
-	printk(KERN_ALERT "echo-serv module loaded\n");
+	svc->thread = kthread_run((void *)start_sending, NULL, "echo-client");
+	printk(KERN_ALERT "echo-client module loaded\n");
 
         return 0;
 }
 
 static void __exit mod_exit(void)
 {
-	if (svc->listen_socket != NULL) {
-		kernel_sock_shutdown(svc->listen_socket, SHUT_RDWR);
-		svc->listen_socket->ops->release(svc->listen_socket);
-		printk(KERN_ALERT "release socket\n");
-	}
+	//if (svc->socket != NULL) {
+	//	kernel_sock_shutdown(svc->socket, SHUT_RDWR);
+	//	sock_release(svc->socket);
+	//	printk(KERN_ALERT "release socket\n");
+	//}
 	
 	kthread_stop(svc->thread);
 
 	kfree(svc);
-	printk(KERN_ALERT "removed echo-serv module\n");
+	printk(KERN_ALERT "removed client-serv module\n");
 }
 
 module_init(mod_init);
 module_exit(mod_exit);
 
-MODULE_DESCRIPTION("TCP/UDP echo server in the kernel");
+MODULE_DESCRIPTION("TCP/UDP echo client in the kernel");
 
 MODULE_LICENSE("GPL");
 
